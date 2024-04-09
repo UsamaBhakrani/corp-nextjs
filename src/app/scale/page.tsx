817d4cb27d339d94662579f02b9a9b5d5c37fc2d
Usama Bhakrani <usamabhakrani@gmail.com>
2024-04-09 21:32:56 +0500

    Routing and Link Comp

---
+++ src/app/scale/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const ScalePage = () => {
+  return (
+    <div>
+      ScalePage
+   
+    </div>
+  );
+};
+
+export default ScalePage;
