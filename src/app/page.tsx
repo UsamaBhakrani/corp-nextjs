dc6b72a8692fb6e23f401017c6e1504e239140c7
Usama Bhakrani <usamabhakrani@gmail.com>
2024-04-09 21:39:23 +0500

    using layout.tsx to render nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <nav>
-        <div>Home Page</div>
-        <Link href="/performance">performance</Link>
-        <Link href="/scale">scale</Link>
-        <Link href="/reliability">reliability</Link>
-      </nav>
+      <div>Home Page</div>
     </div>
   );
 }
