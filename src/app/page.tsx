74923c5d0f133146219a3dffbc56a07fd38ae549
Usama Bhakrani <usamabhakrani@gmail.com>
2024-04-10 02:41:22 +0500

    assests and image comp from next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+import home from "../../public/home.jpg";
 
 export default function Home() {
   return (
     <div>
-      <div>Home Page</div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={home}
+          alt="HomePage Image"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
     </div>
   );
 }
